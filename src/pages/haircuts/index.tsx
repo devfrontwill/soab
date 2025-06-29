02c2396c315f6097370259d5df559aa2a3960266
devfrontwill <devfrontwill@gmail.com>
2025-06-28 21:38:44 -0300

    Style: Feito novas features no componente de modelos de corte

--- src/pages/haircuts/index.tsx
+++ src/pages/haircuts/index.tsx
@@ -2,11 +2,12 @@ import Head from "next/head";
 import { Sidebar } from "@/components/sidebar";
 import { Flex, Text, Heading, Button, Stack, Switch, useMediaQuery } from "@chakra-ui/react";
 import Link from "next/link";
+import { IoMdPricetag } from "react-icons/io";
 
 export default function Haircuts() {
 
-const [isMobile] = useMediaQuery("(max-width: 500px)");
-    
+    const [isMobile] = useMediaQuery("(max-width: 500px)");
+
     return (
         <>
             <Head>
@@ -15,15 +16,15 @@ const [isMobile] = useMediaQuery("(max-width: 500px)");
             <Sidebar>
                 <Flex direction="column" alignItems="flex-start" justifyContent="flex-start">
 
-                    <Flex                        
-                        direction={ isMobile ? "column" : "row" }
+                    <Flex
+                        direction={isMobile ? "column" : "row"}
                         w="100%"
-                        alignItems={ isMobile ? "flex-start" : "center" }
+                        alignItems={isMobile ? "flex-start" : "center"}
                         justifyContent="flex-start"
                         mb={0}
                     >
                         <Heading
-                            fontSize="3xl"
+                            fontSize={isMobile ? "26px" : "3xl"}
                             mt={4}
                             mb={4}
                             mr={4}
@@ -46,6 +47,34 @@ const [isMobile] = useMediaQuery("(max-width: 500px)");
                             />
                         </Stack>
                     </Flex>
+
+                    <Link href="/haircuts/123">
+                        <Flex
+                            cursor="pointer"
+                            w="79vw"
+                            mt={ isMobile ? "2rem" : "1rem" }
+                            p={4}
+                            bg="barber.400"
+                            direction="row"
+                            rounded="4"
+                            mb={2}
+                            justifyContent="space-between"
+                        >
+
+                            <Flex direction="row" alignItems="center" justifyContent="center" >
+                                <IoMdPricetag size={28} color="#fba931" />
+                                <Text fontWeight="bold" ml={4} noOfLines={2} color="white">
+                                    Corte completo
+                                </Text>
+                            </Flex>
+
+                            <Text fontWeight="bold" color="white">
+                                Preço: R$ 59.90
+                            </Text>
+
+                        </Flex>
+                    </Link>
+
                 </Flex>
             </Sidebar>
         </>
